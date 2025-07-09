90f759e9e22ac1badf1d913129b75ac0a999cf77
Francine Dalida <francine.dalida@gmail.com>
2025-07-10 01:01:41 +0800

    Added: more conditions

--- pass-game/src/App.jsx
+++ pass-game/src/App.jsx
@@ -2,6 +2,33 @@ import { useState, useEffect } from "react";
 
 import "./App.css";
 
+const marvelCharacters = [
+  "ironman",
+  "spiderman",
+  "thor",
+  "loki",
+  "thanos",
+  "wanda",
+  "vision",
+  "hawkeye",
+  "hulk",
+  "antman",
+  "blackwidow",
+  "doctorstrange",
+  "falcon",
+  "shuri",
+  "gamora",
+  "drax",
+  "star-lord",
+  "rocket",
+  "nebula",
+  "bucky",
+  "kang",
+  "america",
+  "groot",
+  "moonknight",
+];
+
 const conditionsList = [
   {
     id: 1,
@@ -10,13 +37,77 @@ const conditionsList = [
   },
   {
     id: 2,
-    description: "must include a number",
+    description: "Must include a number",
     check: (pw) => /\d/.test(pw),
   },
   {
     id: 3,
-    description: "Must include a an uppercase letter",
+    description: "Must include an uppercase letter",
     check: (pw) => /[A-Z]/.test(pw),
+  },
+  {
+    id: 4,
+    description: "Must include a special character",
+    check: (pw) => /[!@#$%^&*(),.?":{}|<>]/.test(pw),
+  },
+  {
+    id: 5,
+    description: "Inlude the best song of Chappell Roan",
+    check: (pw) => pw.toLowerCase().includes("casual"),
+  },
+  {
+    id: 7,
+    description: "Must be a palindrome",
+    check: (pw) => pw === pw.split("").reverse().join(""),
+  },
+  {
+    id: 8,
+    description: "Must have exactly 3 numbers",
+    check: (pw) => (pw.match(/\d/g) || []).length === 3,
+  },
+  {
+    id: 9,
+    description: "Must contain at least 2 emojis",
+    check: (pw) => (pw.match(/[\u{1F600}-\u{1F64F}]/gu) || []).length >= 2,
+  },
+  {
+    id: 10,
+    description: "Must not contain the letter 'e'",
+    check: (pw) => !pw.toLowerCase().includes("e"),
+  },
+  {
+    id: 11,
+    description: "Must include today’s weekday",
+    check: (pw) => {
+      const today = new Date()
+        .toLocaleDateString("en-US", { weekday: "long" })
+        .toLowerCase();
+      return pw.toLowerCase().includes(today);
+    },
+  },
+  {
+    id: 12,
+    description: "Must include your favorite hex color code (e.g. #a1b2c3)",
+    check: (pw) => /#[0-9a-fA-F]{6}/.test(pw),
+  },
+  {
+    id: 13,
+    description: "Must include the capital of England but backwards",
+    check: (pw) => pw.toLowerCase().includes("nodnol"),
+  },
+  {
+    id: 14,
+    description: "Must include the year Titanic was released",
+    check: (pw) => pw.includes("1997"),
+  },
+  {
+    // .some() checks if at least one of the characters is included
+    id: 15,
+    description: "Must include a Marvel character",
+    check: (pw) =>
+      marvelCharacters.some((character) =>
+        pw.toLowerCase().includes(character)
+      ),
   },
   // more conditions!!!!!
 ];
